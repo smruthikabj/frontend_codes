93caa789f29284daa94ad744add78f6bd9628b0c
smruthikabj <smruthibommetty28@gmail.com>
2025-08-05 20:41:25 +0530

    To-do app files added

---
+++ React.js/todo-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './pages/Home';
+import './style.css';
+
+function App() {
+
+  return (
+    <div className = "app">
+      <h1>TO-DO APP</h1>
+      <Home/>
+    </div>
+  );
+}
+
+export default App
